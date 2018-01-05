6548b1f200908148723cbdf6c39b3e80a65a2976
Leo <fenge21@qq.com>
2018-01-06 00:16:03 +0800

    添加 ChatterHandler

--- robot.py
+++ robot.py
@@ -1,18 +1,12 @@
 # -*- coding:utf-8 -*-
 
-from wx_robot.handler import MonitorHandler, XiaodouHandler
+from wx_robot.handler import MonitorHandler, XiaodouHandler, ChatterHandler
 from wx_robot.robot import WxRobot
 
 
 if __name__ == '__main__':
     robot = WxRobot(hot_reload=True)
-
-    monitor = MonitorHandler()
-    robot.register_handler(monitor)
-
-    xiaodou_key = 'ZDVRRGo2NWR3RGdUVEFlPUE1QXQvPVRlWXhJQUFBPT0'
-    xiaodou = XiaodouHandler(xiaodou_key)
-    robot.register_handler(xiaodou)
-
+    robot.register_handler(MonitorHandler())
+    robot.register_handler(ChatterHandler()) #robot.register_handler(XiaodouHandler('ZDVRRGo2NWR3RGdUVEFlPUE1QXQvPVRlWXhJQUFBPT0'))
     robot.run()
 
